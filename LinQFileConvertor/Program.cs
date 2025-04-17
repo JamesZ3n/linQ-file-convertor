016e8c12e2e76f0fd5c6d7bd1e544f4675ea63af
EternalNico <nicolas.sacilotto22@gmail.com>
2025-04-17 14:29:59 +0200

    add json to xml converter

--- LinQFileConvertor/Program.cs
+++ LinQFileConvertor/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
 JsonToXmlConverter.Convertor();
-=======
-JsonToXmlConverter.Main();
->>>>>>> d66e093a1785acd2f3da32dd9dfbdcd193235002
